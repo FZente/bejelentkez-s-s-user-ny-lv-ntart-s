870aa0d07aff01221935b48a5f6acd53a40ee695
Fekete Zente <c0fekzen@vasvari.org>
2025-09-10 15:47:00 +0200

    Működő hibaüzenetek

--- login.js
+++ login.js
@@ -3,13 +3,15 @@ function registration() {
   let reg_pass = document.getElementById("reg_pass").value;
   let reg_rep_pass = document.getElementById("reg_rep_pass").value;
 
-  let joEmail = reg_email.include("@");
+  let joEmail = reg_email.includes("@");
 
-  if (joEmail == false) {
+  if (!joEmail) {
     alert("Helytelen email vagy jelszó!");
+    return;
   }
 
-  if (reg_pass != reg_rep_pass) {
+  if (reg_pass !== reg_rep_pass) {
     alert("Helytelen email vagy jelszó!");
+    return;
   }
 }
